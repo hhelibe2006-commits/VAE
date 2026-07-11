d1cc8cd0ac2cde7d1edb024b2b1b3eec1cb88c70
LIU JIAHAO <hhelibe2006@gmail.com>
2026-07-11 13:04:54 +0800

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,172 @@
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+"""
+先添加统一隐藏维度为z_dim
+再添加device = torch.device("cuda" if torch.cuda.is_available() else "cpu")，并将所有使用cuda方法的对象改为to(device)
+"""
+
+seed = 0
+
+random.seed(seed)
+torch.manual_seed(seed)
+
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu")
+z_dim = 16
+ann = 50
+bs = 100
+# MNIST Dataset
+train_dataset = datasets.MNIST(root='./mnist_data/', train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.MNIST(root='./mnist_data/', train=False, transform=transforms.ToTensor(), download=False)
+
+# Data Loader (Input Pipeline)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=bs, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=bs, shuffle=False)
+
+class ConvVAE(nn.Module):
+    def __init__(self, latent_dim=20):
+        super(ConvVAE, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+        self.fc_mu = nn.Linear(128 * 7 * 7, latent_dim)
+        self.fc_log = nn.Linear(128 * 7 * 7, latent_dim)
+
+        self.decoder_input = nn.Linear(latent_dim, 128 * 7 * 7)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 1, kernel_size=3, stride=1, padding=1),
+            nn.Sigmoid()
+        )
+
+    def encode(self, x):
+        h = self.encoder(x)
+        mu = self.fc_mu(h)
+        log_var = self.fc_log(h)
+        return mu, log_var
+
+    def decode(self, z):
+        h = self.decoder_input(z)
+        h = h.view(-1, 128, 7, 7)
+        x_recon = self.decoder(h)
+        return x_recon
+
+    def sampling(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.sampling(mu, log_var)
+        x_recon = self.decode(z)
+        return x_recon, mu, log_var
+
+class VAE(nn.Module):
+    def __init__(self, x_dim, h_dim1, h_dim2, z_dim):
+        super(VAE, self).__init__()
+        
+        # encoder part
+        self.fc1 = nn.Linear(x_dim, h_dim1)
+        self.fc2 = nn.Linear(h_dim1, h_dim2)
+        self.fc31 = nn.Linear(h_dim2, z_dim)
+        self.fc32 = nn.Linear(h_dim2, z_dim)
+        # decoder part
+        self.fc4 = nn.Linear(z_dim, h_dim2)
+        self.fc5 = nn.Linear(h_dim2, h_dim1)
+        self.fc6 = nn.Linear(h_dim1, x_dim)
+        
+    def encoder(self, x):
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        return self.fc31(h), self.fc32(h) # mu, log_var
+    
+    def sampling(self, mu, log_var):
+        std = torch.exp(0.5*log_var)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu) # return z sample
+        
+    def decoder(self, z):
+        h = F.relu(self.fc4(z))
+        h = F.relu(self.fc5(h))
+        return F.sigmoid(self.fc6(h)) 
+    
+    def forward(self, x):
+        mu, log_var = self.encoder(x.view(-1, 784))
+        z = self.sampling(mu, log_var)
+        return self.decoder(z), mu, log_var
+
+# build model
+#vae = VAE(x_dim=784, h_dim1= 512, h_dim2=256, z_dim=z_dim).to(device)
+vae = ConvVAE(latent_dim=z_dim).to(device)
+optimizer = optim.Adam(vae.parameters())
+
+# return reconstruction error + KL divergence losses
+def loss_function(recon_x, x, mu, log_var,bate =1.0):
+    BCE = F.binary_cross_entropy(
+        recon_x.view(-1, 784), x.view(-1, 784), reduction='sum'
+    )
+    KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+    return BCE + KLD*bate
+
+def train(epoch):
+    vae.train()
+    train_loss = 0
+    bate = min(2.0, epoch / ann)
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        
+        recon_batch, mu, log_var = vae(data)
+        loss = loss_function(recon_batch, data, mu, log_var, bate)
+        
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item() / len(data)))
+    print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(train_loader.dataset)))
+
+def test():
+    vae.eval()
+    test_loss= 0
+    with torch.no_grad():
+        for data, _ in test_loader:
+            data = data.to(device)
+            recon, mu, log_var = vae(data)
+            
+            # sum up batch loss
+            test_loss += loss_function(recon, data, mu, log_var).item()
+        
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+for epoch in range(1, 100):
+    train(epoch)
+    test()
+
+with torch.no_grad():
+    z = torch.randn(64, z_dim).to(device)
+    sample = vae.decode(z).to(device)
+    
+    save_image(sample.view(64, 1, 28, 28), './samples/sample_' + '.png')
